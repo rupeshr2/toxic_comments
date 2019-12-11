c5519aba2c73797ada94d7599f28bed357628f5f
rupeshr2 <53382771+rupeshr2@users.noreply.github.com>
2019-12-11 11:21:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -61,4 +61,4 @@ def run_prediction(request: Request, comment: str = Form(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
